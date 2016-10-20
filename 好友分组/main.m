e5fe467db3faa0b24f6d4223b7478cf975eb44f3
Lili <lili2011050508@163.com>
2016-10-20 15:39:35 +0800

    Initial Commit

---
+++ 好友分组/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  好友分组
+//
+//  Created by LiliEhuu on 16/10/20.
+//  Copyright © 2016年 LiliEhuu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
